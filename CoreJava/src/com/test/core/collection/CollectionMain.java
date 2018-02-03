3ec083e50d6d101498863d5969d24f8c3cdbf58d
venkatkalluri <kalluri.venkata@gmail.com>
2018-02-03 12:00:20 -0600

    initial checkin

---
+++ CoreJava/src/com/test/core/collection/CollectionMain.java
@@ -0,0 +1,41 @@
+package com.test.core.collection;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class CollectionMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		CollectionList cl = new CollectionList();
+		cl.perfOfArrayList();
+		cl.perfOfLinkedList();
+	}
+
+}
+
+class CollectionList{
+	
+	List<String> al = new ArrayList<String>();
+	List<String> ll = new LinkedList<String>();
+	void perfOfArrayList(){
+		long startTime = System.currentTimeMillis();
+		for(int i=0;i<2145007;i++) {
+			al.add(String.valueOf(i));
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Time Taken for ArrayList::"+(endTime-startTime)+" milli seconds..");
+	}
+	
+	void perfOfLinkedList(){
+		long startTime = System.currentTimeMillis();
+		for(int i=0;i<2145007;i++) {
+			ll.add(String.valueOf(i));
+		}
+		long endTime = System.currentTimeMillis();
+		System.out.println("Time Taken for LinkedList::"+(endTime-startTime)+" milli seconds..");
+		
+	}
+
+}
