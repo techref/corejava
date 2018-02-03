f255507138f8bd80bb29ec5aeb016ead521f7283
venkatkalluri <kalluri.venkata@gmail.com>
2018-02-03 12:11:42 -0600

    1st change

--- CoreJava/src/com/test/core/collection/CollectionMain.java
+++ CoreJava/src/com/test/core/collection/CollectionMain.java
@@ -8,6 +8,7 @@ public class CollectionMain {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("1st Git Change...");
 		CollectionList cl = new CollectionList();
 		cl.perfOfArrayList();
 		cl.perfOfLinkedList();
